4d862bdfdcad8fb180a074e8013c5018dac27647
Brenton Little <brentonlittle@Brentons-MacBook-Pro.local>
2016-04-09 19:03:12 +0930

    added files

--- superCool/ViewController.swift
+++ superCool/ViewController.swift
@@ -10,6 +10,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var hideRedButton: UIButton!
+    @IBOutlet weak var hideBlueButton: UIButton!
+ 
+    @IBOutlet weak var bh: UIImageView!
+    @IBOutlet weak var rh: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +25,30 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func hideRedHat(sender: AnyObject) {
+        if rh.hidden == false {
+            rh.hidden = true
+            hideRedButton.setTitle("Show Red", forState: .Normal)
+        }
+        else {
+            rh.hidden = false
+            hideRedButton.setTitle("Hide Red", forState: .Normal)
+            
+        }
+        
+    }
 
+    @IBAction func hideBlueHat(sender: AnyObject) {
+        if bh.hidden == false {
+            bh.hidden = true
+            hideBlueButton.setTitle("Show Blue", forState: .Normal)
+        }
+        else {
+            bh.hidden = false
+            hideBlueButton.setTitle("Hide blue", forState: .Normal)
+            
+        }
+        
+    }
 }
 
